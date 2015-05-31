35c28b78c6a667bab71261e1bc781e5d5073c342
HeisenbergWithoutHat <pcfeng502@gmail.com>
2015-05-31 10:08:02 +0800

    fixed a minor bug in polls/models.py
    1. B4 fixing.
    history: votes = models.IntegerField(0), which results in the admin
    votes page that the “Vote” column will be shown as “0”.
    now: votes = models.IntegerField(default=0)

--- polls/models.py
+++ polls/models.py
@@ -19,7 +19,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(0)
+    votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
 
